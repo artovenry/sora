66093e8c7a057139dad84ed92c4526ec36a09179
artovenry <artovenry@gmail.com>
2015-11-19 18:37:43 +0900

    add Helper base class

---
+++ lib/Artovenry/Sora/Helper.php
@@ -0,0 +1,18 @@
+<?
+namespace Artovenry\Sora;
+abstruct class Helper{
+	static function helpers(){
+    $helper_klass= new ReflectionClass(get_called_class());
+    $rs= [];
+    foreach($helper_klass->getMethods(ReflectionMethod::IS_PUBLIC) as $item){
+    	if(
+    		$item->isStatic() ||
+    		$item->isConstructor() ||
+    		$item->isAbstract()
+    	)continue;
+    	$helper_name= "_" . $item->name;
+    	$rs[$helper_name]= $item->getClosure(new static);
+    }
+		return $rs;
+  }
+}
