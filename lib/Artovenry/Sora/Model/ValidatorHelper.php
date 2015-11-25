06996fc44068de701b84274460d7f6cacf9ec77d
artovenry <artovenry@gmail.com>
2015-11-25 20:35:34 +0900

    commit

--- lib/Artovenry/Sora/Model/ValidatorHelper.php
+++ lib/Artovenry/Sora/Model/ValidatorHelper.php
@@ -3,16 +3,18 @@ namespace Artovenry\Sora\Model;
 
 trait ValidatorHelper{
   protected function is_duplicated($attr_name, $val, $options=[]){
-  	$scopes= empty($options["scopes"])? $options["scopes"]: [];
-  	$record= $this;
+    $scopes= empty($options["scopes"])? []: $options["scopes"];
+    $record= $this;
     $selected= array_reduce(
-    	$scopes,
-    	function($rs,$item) use($record){
-    		return $rs->where($item, $record->$item);
-    	},
-    	self::factory(get_class($this))
+      $scopes,
+      function($rs,$item) use($record){
+        return $rs->where($item, $record->$item);
+      },
+      self::factory(get_class($this))
     )->where($attr_name, $val)->take();
 
+    var_dump(\ORM::get_query_log());
+    exit; 
     if($selected == false)return false;
     if($this->is_new())return true;
     if($selected->id == $this->id)return false;
